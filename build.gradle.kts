21629fb33f4d2cbfefbf82b056a0830076a39719
green <alan_greens@outlook.com>
2024-06-03 21:12:07 +0800

    [feat] new module recall-repl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,48 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.6"
-	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm") version "1.9.24"
-	kotlin("plugin.spring") version "1.9.24"
-	kotlin("plugin.jpa") version "1.9.24"
+	id("org.springframework.boot") version "3.2.6" apply false
+	id("io.spring.dependency-management") version "1.1.5" apply false
+	kotlin("jvm") version "1.9.24" apply false
+	kotlin("plugin.spring") version "1.9.24" apply false
+	kotlin("plugin.jpa") version "1.9.24" apply false
 }
 
 group = "cc.green"
-version = "0.0.1-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-}
+subprojects {
 
-repositories {
-	mavenLocal()
-	maven {
-		url = uri("https://maven.aliyun.com/repository/central/")
+	apply(plugin = "java-library")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+
+	repositories {
+		mavenLocal()
+		maven {
+			url = uri("https://maven.aliyun.com/repository/central/")
+		}
+		maven {
+			url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+		}
+		mavenCentral()
 	}
-	maven {
-		url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+
+	tasks.withType<JavaCompile> {
+		sourceCompatibility = JavaVersion.VERSION_17.toString()
 	}
-	mavenCentral()
-}
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs += "-Xjsr305=strict"
+			jvmTarget = "17"
+		}
 	}
-}
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
 }
---
+++ recall-server/build.gradle.kts
@@ -0,0 +1,12 @@
+version = "0.0.1-SNAPSHOT"
+
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
