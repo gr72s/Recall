db657a57eb3d1244449f75845d5978d0ae4f494c
green <alan_greens@outlook.com>
2024-06-08 00:16:01 +0800

    update id_sequence sql

--- sql/update_id_sequence.sql
+++ sql/update_id_sequence.sql
@@ -1,5 +1,5 @@
-select setval('cs_platform_id_seq', (select max(id) from cs_platform) + 1);
-select setval('cs_record_id_seq', (select max(id) from cs_record) + 1);
-select setval('cs_tag_id_seq', (select max(id) from cs_tag) + 1);
-select setval('pay_account_id_seq', (select max(id) from pay_account) + 1);
-select setval('pay_platform_id_seq', (select max(id) from pay_platform) + 1);
+select setval('cs_platform_id_seq', select max(id) from cs_platform);
+select setval('cs_record_id_seq', select max(id) from cs_record);
+select setval('cs_tag_id_seq', select max(id) from cs_tag);
+select setval('pay_account_id_seq', select max(id) from pay_account;
+select setval('pay_platform_id_seq', select max(id) from pay_platform));
