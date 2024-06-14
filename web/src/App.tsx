e14a724cc5533d30104519844bbef6d278c546e2
green <alan_greens@outlook.com>
2024-06-15 00:01:11 +0800

    chore: add prettier && add material ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import FullFeaturedCrudGrid from "./FullFeaturedCrudGrid";
 
 function App() {
   return (
-    <div />
+    <div>
+      <FullFeaturedCrudGrid />
+    </div>
   );
 }
 
